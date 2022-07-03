8155711b6f33d6b9ecab810ef9d2979d7fc93a45
rodri_rod <47514098+rodrigotorres24@users.noreply.github.com>
2022-07-02 19:17:08 -0700

    Add files via upload

---
+++ IotMain.java
@@ -0,0 +1,20 @@
+package iot.internetofthings;
+
+
+public class IotMain{
+
+   
+
+    public static void main(String[] args){
+
+    }
+
+
+
+//Instanciamos Objetos.
+   public void addDevice(){
+   
+
+    
+   }
+}
